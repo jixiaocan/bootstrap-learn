6fb07dcb12dc75b8758be946030d52a029f0919e
kylematt <837629224@qq.com>
2017-03-04 12:22:07 +0800

    first time add, just two simple bootstrap htmls.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # bootstrap-learn
 use bootstrap to build a simple web
+
+index-bs.html和movie-bs.html是用Bootstrap写的两个简单页面。
+
---
+++ scripts/custom.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carousel-pause").click(function() {
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function() {
+        $("#mycarousel").carousel('cycle');
+    });
+
+    $(".feed-weixin").popover({
+        placement: "top",
+        trigger: "hover",
+        container: "body",
+        html: true
+    });
+
+    $(".feed-weibo, .feed-tqq, .feed-rss").tooltip({
+        container: "body"
+    });
+
+    /*回到顶部功能*/
+    $("body").append('<div class="rollto"><a href="javascript:;"></a></div>');
+    $(document).on("click", '.rollto a', function() {
+        $("html,body").animate({
+            scrollTop: 0
+        });
+    });
+    $(window).scroll(function() {
+        var a = $(".rollto");
+        if (document.documentElement.scrollTop + document.body.scrollTop > 200) {
+            a.fadeIn();
+        } else {
+            a.fadeOut();
+        }
+    });
+
+});
